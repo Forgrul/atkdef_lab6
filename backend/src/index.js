a50465b0bcf554f24ba90ad2f9a92a11a98fd713
Forgrul <hngieng9@gmail.com>
2023-03-28 15:00:48 +0800

    init

---
+++ backend/src/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+const port = process.env.PORT || 8000;
+const app = express();
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
