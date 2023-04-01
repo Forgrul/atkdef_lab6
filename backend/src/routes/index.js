6335d8c800b59afa7bf3842e7eb719e3b050c414
Forgrul <hngieng9@gmail.com>
2023-04-01 18:25:48 +0800

    fix csrf not working (because .env file)

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,20 +1,24 @@
 import express from "express";
 import glob from "glob";
+import users from "./api/v1/users";
+import v1 from "./api/v1";
 
 const rootRouter = express.Router();
 
-async function autoloadRoutes() {
-  const jsfiles = await glob("**/index.js", {
-    cwd: "src/routes",
-    ignore: "index.js",
-  });
-  const importTasks = jsfiles.map(async (path) => {
-    const module = await import(`./${path}`);
-    if (module.default === undefined) return;
-    rootRouter.use(`/${path.slice(0, -9)}`, module.default);
-  });
-  return Promise.all(importTasks);
-}
-await autoloadRoutes();
+rootRouter.use("/api/v1/users", users);
+rootRouter.use("/api/v1", v1);
+// async function autoloadRoutes() {
+//   const jsfiles = await glob("**/index.js", {
+//     cwd: "src/routes",
+//     ignore: "index.js",
+//   });
+//   const importTasks = jsfiles.map(async (path) => {
+//     const module = await import(`./${path}`);
+//     if (module.default === undefined) return;
+//     rootRouter.use(`/${path.slice(0, -9)}`, module.default);
+//   });
+//   return Promise.all(importTasks);
+// }
+// await autoloadRoutes();
 
 export default rootRouter;
