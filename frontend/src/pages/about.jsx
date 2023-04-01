8a5b568573e22ea5e42686474c68f5f5de6652a0
Forgrul <hngieng9@gmail.com>
2023-04-01 18:32:07 +0800

    done?

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -1,3 +1,9 @@
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <div className="my-16">
+        <div className="flex justify-center my-4">
+            <h1 className="text-4xl block">嚴詩順(B10902100) from CSIE</h1>
+        </div>
+    </div>
+  )
 }
